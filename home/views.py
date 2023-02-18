ae41d1c7092796548bfa8fbb4be1e3e7c3749cb5
domfernnan <57579248+domfennan@users.noreply.github.com>
2023-02-18 20:11:24 -0300

    criado o home e login
    bora

--- home/views.py
+++ home/views.py
@@ -3,4 +3,18 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
+
+from datetime import date
+from django.shortcuts import render
+
+def home(request):
+    data_atual = date.today()
+    jogos = [
+        {'time': 'Barcelona x Real Madrid', 'horario': '12h00'},
+        {'time': 'Arsenal x Liverpool', 'horario': '15h00'},
+        {'time': 'Flamengo x Palmeiras', 'horario': '18h00'}
+    ] # lista de jogos previstos para o dia
+    return render(request, 'home.html', {'data_atual': data_atual, 'jogos': jogos})
+
+
 # Create your views here.
